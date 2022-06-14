e1117a2d6c948b697a52e01e26d7743ec7f4cd59
Sabrina Drashen <sdrashen@gmail.com>
2022-06-13 22:57:06 -0300

    Projeto finalizado.

--- main.js
+++ main.js
@@ -21,6 +21,49 @@ function onScroll() {
     //onScroll function tem apenas 1 função: gerenciar os eventos de scroll da page
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+//innerHeight no dev tolls mostra a altura visível da page
+//innerHeight / 2 mostra a altura da page dividida ao meio
+//innerHeight / 2 + scrollY
+function activateMenuAtCurrentSection() {
+    //Aqui estamos recriando o valor do target line, por isso usando const
+    const targetLine = scrollY + innerHeight / 2
+        //"Estado da aplicação" muda conforme o scroll
+        //Abaixo: verificar se a sessão passou da linha. Quais dados serão necessários?
+        //console.log(home.offsetTop) do id home, ñ necessita #. Se fosse, por exemplo, #home-section o JS ñ conseguiria identifcar. Isso aqui foi apenas para ensinar.
+    const sectionTop = home.offsetTop
+    const sectionHeight = home.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    //infos do dados: e da lógica. No console.log, no dev tolls, aparece true or false
+    //console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine)
+    //Abaixo: verificar se a base está abaixo da linha. Quais dados serão necessários?
+    //console.log(sectionHeight)
+    //A seção termina onde?
+    const sectionEndsAt = sectionTop + sectionHeight
+        // console.log(sectionEndsAt)
+
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+        //console.log('O fundo de seção passou da linha?, sectionEndPassedTargetline')
+        //Necessário que se transforme em true
+
+    const sectionBoundaries =
+        sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
+        //O ! nega o booleano
+        // console.log(sectionBoundaries)
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(
+        `.menu a[href*=${section.getAttribute(id)}]`
+    )
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
